2d957dee633fd1967561b19947588bcaee023901
Patrice Andala <patriceandala@gmail.com>
2025-06-19 18:07:57 +0300

    Added Main

--- build.sbt
+++ build.sbt
@@ -1,7 +1,15 @@
 name := "FunctionalFinance"
 version := "0.1"
 scalaVersion := "2.13.12"
+
+
 libraryDependencies ++= Seq(
-  "org.typelevel" %% "cats-core" % "2.10.0",
-  "org.scalatest" %% "scalatest" % "3.2.16" % Test
+  "org.http4s" %% "http4s-blaze-server" % "0.23.23",
+  "org.http4s" %% "http4s-dsl" % "0.23.30",
+  "org.http4s" %% "http4s-circe" % "0.23.30",
+  "io.circe" %% "circe-generic" % "0.14.14",
+  "org.typelevel" %% "cats-effect" % "3.6.1",
+  "org.typelevel" %% "cats-core" % "2.13.0",
+  "org.scalatest" %% "scalatest" % "3.2.19" % Test
+
 )
--- src/main/scala/com/patriceandala/finance/Main.scala
+++ src/main/scala/com/patriceandala/finance/Main.scala
@@ -1,11 +1,88 @@
 package com.patriceandala.finance
 
+
+import cats.effect._
 import cats.implicits._
+import org.http4s._
+import org.http4s.circe._
+import org.http4s.dsl.io._
 
-object Main extends App {
-  println("Welcome to FunctionalFinance!")
+import scala.io.circe.generic.auto._
 
-  val transactions = List(200.0, -100.0, 50.0, -30.0)
-  val balance = transactions.combineAll
-  println(s"Total balance: $$balance")
+final case class User(id: String, username: String, password: String, balance: Double)
+final case class AuthRequest(username: String, password: String)
+final case class BalanceResponse(balance: Double)
+final case class MoneyRequest(amount: Double)
+
+object Main extends IOApp {
+  def run(args: List[String]): IO[ExitCode] = {
+    Ref.of[IO, List[User]](List.empty).flatMap { userDb =>
+      val service = new UserService(userDb)
+      BlazeServerBuilder[IO]
+        .bindHttp(8080, "0.0.0.0")
+        .withHttpApp(service.routes.orNotFound)
+        .serve
+        .compile
+        .drain
+        .as(ExitCode.Success)
+    }
+  }
+}
+
+class UserService(userDb: Ref[IO, List[User]]) {
+  implicit val decoder = jsonOf[IO, AuthRequest]
+  implicit val moneyDecoder = jsonOf[IO, MoneyRequest]
+
+  val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
+    case req @ POST -> Root / "signup" =>
+      for {
+        auth <- req.as[AuthRequest]
+        id = java.util.UUID.randomUUID().toString
+        newUser = User(id, auth.username, auth.password, 0.0)
+        _ <- userDb.update(users => newUser :: users)
+        res <- Ok(s"User created with ID: $id")
+      } yield res
+
+    case req @ POST -> Root / "login" =>
+      for {
+        auth <- req.as[AuthRequest]
+        users <- userDb.get
+        maybeUser = users.find(u => u.username == auth.username && u.password == auth.password)
+        res <- maybeUser match {
+          case Some(user) => Ok(s"Welcome ${user.username}!")
+          case None       => Unauthorized("Invalid credentials")
+        }
+      } yield res
+
+    case GET -> Root / "users" =>
+      userDb.get.flatMap(users => Ok(users.map(u => s"${u.id}: ${u.username} (${u.balance})").mkString("\n")))
+
+    case GET -> Root / "balance" / userId =>
+      for {
+        users <- userDb.get
+        maybeUser = users.find(_.id == userId)
+        res <- maybeUser match {
+          case Some(user) => Ok(BalanceResponse(user.balance))
+          case None       => NotFound("User not found")
+        }
+      } yield res
+
+    case req @ POST -> Root / "deposit" / userId =>
+      for {
+        money <- req.as[MoneyRequest]
+        updated <- userDb.modify { users =>
+          users.find(_.id == userId) match {
+            case Some(user) =>
+              val newUser = user.copy(balance = user.balance + money.amount)
+              val updatedList = users.map(u => if (u.id == userId) newUser else u)
+              (updatedList, Some(newUser))
+            case None => (users, None)
+          }
+        }
+        res <- updated match {
+          case Some(user) => Ok(BalanceResponse(user.balance))
+          case None       => NotFound("User not found")
+        }
+      } yield res
+  }
 }
